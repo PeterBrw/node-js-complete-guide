645a08889f2d9afde30eec958c042b72c188c174
Petru <peter.giurgiu@gmail.com>
2019-01-10 12:49:51 +0200

    182. Fetching All Products

--- models/product.js
+++ models/product.js
@@ -19,6 +19,21 @@ class Product {
         console.log(err);
       });
   }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
+        console.log(err);
+      }); // this 'find()' it will return all the documents from the collection 'products', and it will not return a 'promise' it will return 'cursor;(an object provided by mongodb), all '.toArray()' it will turn all documents into JS array(we should use this just if we know that there are maximum 100 documents in our collection, otherwise is good to use 'pagination'(we will use it later in the course))
+  } 
 }
 
 module.exports = Product;
