f93bc3bfa9cc40e3d666f5bcf99e6bda9214c33f
Petru <peter.giurgiu@gmail.com>
2019-01-17 14:01:34 +0200

    219. Deleting Cart Items

--- models/user.js
+++ models/user.js
@@ -2,26 +2,30 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({ 
-  name: { 
+const userSchema = new Schema({
+  name: {
     type: String,
     required: true
-  }, 
+  },
   email: {
     type: String,
     required: true
   },
-  cart: { 
+  cart: {
     items: [
       {
-        productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true }, 
-        quantity: { type: Number, required: true } 
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true
+        },
+        quantity: { type: Number, required: true }
       }
     ]
   }
 });
- 
-userSchema.methods.addToCart = function(product) { 
+
+userSchema.methods.addToCart = function(product) {
   const cartProductIndex = this.cart.items.findIndex(cp => {
     return cp.productId.toString() === product._id.toString();
   });
@@ -42,9 +46,17 @@ userSchema.methods.addToCart = function(product) {
   };
   this.cart = updatedCart;
   return this.save();
+};
+
+userSchema.methods.removeFromCart = function(productId) { 
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString(); 
+  });
+  this.cart.items = updatedCartItems; 
+  return this.save(); 
 }
 
-module.exports = mongoose.model('User', userSchema); 
+module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
