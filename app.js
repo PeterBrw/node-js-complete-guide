e905bb934abed0031df23ebfd1221029177333df
Petru <peter.giurgiu@gmail.com>
2019-01-21 18:02:55 +0200

    Assignment. Sessions and Cookies

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session); 
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://maximilian:maximilian@cluster0-5pzzp.mongodb.net/shop'; 
+const MONGODB_URI =
+'mongodb+srv://maximilian:maximilian@cluster0-5pzzp.mongodb.net/shop'
 
 const app = express();
-
-const store = new MongoDBStore({ 
-  uri: MONGODB_URI, 
-  collection: 'sessions' 
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
 });
 
 app.set('view engine', 'ejs');
@@ -29,43 +29,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
-    secret: 'my secret', 
-    resave: false, 
-    saveUninitialized: false, 
-    store: store // this middleware will automatically set a cookie for us and automatically reads the cookie for us as well
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
   })
-); 
-
-app.use((req, res, next) => {
-  User.findById('5c3db72f04b84d2068ef4f14') 
-    .then(user => {
-      req.user = user ; 
-      next();
-    })
-    .catch(err => console.log(err));
-});
+);
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(authRoutes);  
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose 
-  .connect(
-    MONGODB_URI 
-  )
+mongoose
+  .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => { 
-      if(!user) { 
-        const user = new User({ 
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
           name: 'Max',
           email: 'max@test.com',
           cart: {
-            items: [] 
+            items: []
           }
         });
-        user.save(); 
+        user.save();
       }
     });
     app.listen(3000);
