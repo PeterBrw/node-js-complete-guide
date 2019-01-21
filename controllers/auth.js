f32921073fb062def9eec6967701f5d8b2fb30ab
Petru <peter.giurgiu@gmail.com>
2019-01-21 12:15:07 +0200

    233. Configuring Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,17 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req
-  .get('Cookie')
-  .split(';')[0]
-  .trim()
-  .split('=')[1]; 
-  console.log(isLoggedIn)
+  // const isLoggedIn = req
+  // // .get('Cookie')
+  // // .split(';')[0]
+  // // .trim()
+  // // .split('=')[1] === 'true'; 
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true'); 
+  res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
   res.redirect('/');
-};
+}; 
