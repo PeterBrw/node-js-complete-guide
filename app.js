a5fd9a2605cc3d043e22982a56257e06451ffe0e
Petru <peter.giurgiu@gmail.com>
2019-01-21 19:00:50 +0200

    242. Two Tiny Improvments

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  if(!req.session.user){ // if there is no 'user' it will execute the next middleware
+  if(!req.session.user){ 
     return next();
   }
-  User.findById(req.session.user._id) // here the id of the user we are taking it from session
+  User.findById(req.session.user._id) 
     .then(user => {
       req.user = user;
       next();
