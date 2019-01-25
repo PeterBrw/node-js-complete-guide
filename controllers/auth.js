c95a992ea1037784d0d93ce0488ed3ba69c74fe8
Petru <peter.giurgiu@gmail.com>
2019-01-25 17:48:12 +0200

    292. Adding Async Validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,8 +3,8 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
-const { validationResult } = require('express-validator/check'); 
- 
+const { validationResult } = require('express-validator/check');
+
 const User = require('../models/user');
 
 const transporter = nodemailer.createTransport(
@@ -78,47 +78,35 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
-  const errors = validationResult(req); 
-  if(!errors.isEmpty()) { 
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
     console.log(errors.array());
-    return res.status(422).render('auth/signup', { 
+    return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()[0].msg 
+      errorMessage: errors.array()[0].msg
     });
   }
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash(
-          'error',
-          'E-Mail exists already, please pick a different one.'
-        );
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] }
-          });
-          return user.save();
-        })
-        .then(result => {
-          res.redirect('/login');
-          return transporter.sendMail({
-            to: email,
-            from: 'shop@node-complete.com',
-            subject: 'Signup succeeded!',
-            html: '<h1>You successfully signed up!</h1>'
-          });
-        })
-        .catch(err => {
-          console.log(err);
-        });
+
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] }
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+      // return transporter.sendMail({
+      //   to: email,
+      //   from: 'shop@node-complete.com',
+      //   subject: 'Signup succeeded!',
+      //   html: '<h1>You successfully signed up!</h1>'
+      // });
     })
     .catch(err => {
       console.log(err);
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user'); // importin 'User' model
 
 const router = express.Router();
 
@@ -17,24 +18,31 @@ router.post(
     check('email')
       .isEmail()
       .withMessage('Please enter a valid email.')
-      .custom((value, { req }) => { 
-        if (value === 'test@test.com') { 
-          throw new Error('This email address if forbidden.');
-        }
-        return true;
-      }), 
+      .custom((value, { req }) => {
+        // if (value === 'test@test.com') {
+        //   throw new Error('This email address if forbidden.');
+        // }
+        // return true;
+        return User.findOne({ email: value }).then(userDoc => { 
+          if (userDoc) { 
+            return Promise.reject( 
+              'E-Mail exists already, please pick a different one.'
+            );
+          }
+        });
+      }),
     body(
-      'password', 
-       'Please enter a password with only numbers and text and at least 5 characters.' 
-      ) 
-      .isLength({ min: 5 }) 
+      'password',
+      'Please enter a password with only numbers and text and at least 5 characters.'
+    )
+      .isLength({ min: 5 })
       .isAlphanumeric(),
     body('confirmPassword').custom((value, { req }) => {
-      if(value !== req.body.password) {
+      if (value !== req.body.password) {
         throw new Error('Passwords have to match!');
       }
       return true;
-    })      
+    })
   ],
   authController.postSignup
 );
