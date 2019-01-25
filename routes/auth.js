c22dbe64d8f1f965cc1aeee4a58ed846a63663b1
Petru <peter.giurgiu@gmail.com>
2019-01-25 17:13:30 +0200

    290. More Validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -13,15 +13,23 @@ router.post('/login', authController.postLogin);
 
 router.post(
   '/signup',
-  check('email')
-    .isEmail()
-    .withMessage('Please enter a valid email.')
-    .custom((value, { req }) => { 
-      if (value === 'test@test.com') { 
-        throw new Error('This email address if forbidden.');
-      }
-      return true;
-    }),
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => { 
+        if (value === 'test@test.com') { 
+          throw new Error('This email address if forbidden.');
+        }
+        return true;
+      }), 
+    body(
+      'password', // look for the 'password' in the body of the request
+       'Please enter a password with only numbers and text and at least 5 characters.' // default error message for this (password validator)
+      ) 
+      .isLength({ min: 5 }) // minim length of the 'password' to be 5 characters
+      .isAlphanumeric()   // in production we should use something more complex than this
+  ],
   authController.postSignup
 );
 
