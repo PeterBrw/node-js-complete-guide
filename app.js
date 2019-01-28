79ab84ccd53d4ed286693c8fab13b7342140f0b9
Petru <peter.giurgiu@gmail.com>
2019-01-28 22:11:13 +0200

    306. Returning Error Pages

--- app.js
+++ app.js
@@ -47,14 +47,14 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then(user => {
-      if(!user) { 
+      if (!user) {
         return next();
       }
       req.user = user;
       next();
     })
     .catch(err => {
-      throw new Error(err); 
+      throw new Error(err);
     });
 });
 
@@ -67,6 +67,8 @@ app.use((req, res, next) => {
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
+
+app.get('/500', errorController.get500);
 
 app.use(errorController.get404);
 
